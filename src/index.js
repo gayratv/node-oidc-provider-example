add16090744544186f5c19a961e0714ec1764a38
Gayrat Vlasov <ggvlasov@gmail.com>
2022-02-06 14:46:17 +0300

    added signing keys

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// see previous example for the things that are not commented
+
 const assert = require('assert');
 const Provider = require('oidc-provider');
 
@@ -6,8 +8,8 @@ assert(process.env.PORT, 'process.env.PORT missing');
 assert(process.env.SECURE_KEY, 'process.env.SECURE_KEY missing, run `heroku addons:create securekey`');
 assert.equal(process.env.SECURE_KEY.split(',').length, 2, 'process.env.SECURE_KEY format invalid');
 
-// new Provider instance with no extra configuration, will run in default, just needs the issuer
-// identifier, uses data from runtime-dyno-metadata heroku here
+const jwks = require('./jwks.json');
+
 const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`, {
   clients: [
     {
@@ -21,10 +23,8 @@ const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
   cookies: {
     keys: process.env.SECURE_KEY.split(','),
   },
+  jwks,
 });
 
-// Heroku has a proxy in front that terminates ssl, you should trust the proxy.
 oidc.proxy = true;
-
-// listen on the heroku generated port
 oidc.listen(process.env.PORT);
