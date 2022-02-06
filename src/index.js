5376fc1b58dc7713cbc74b02616a09e98c9c8281
Gayrat Vlasov <ggvlasov@gmail.com>
2022-02-06 14:53:28 +0300

    added a redis adapter for persisting data

--- src/index.js
+++ src/index.js
@@ -7,10 +7,16 @@ assert(process.env.HEROKU_APP_NAME, 'process.env.HEROKU_APP_NAME missing');
 assert(process.env.PORT, 'process.env.PORT missing');
 assert(process.env.SECURE_KEY, 'process.env.SECURE_KEY missing, run `heroku addons:create securekey`');
 assert.equal(process.env.SECURE_KEY.split(',').length, 2, 'process.env.SECURE_KEY format invalid');
+assert(process.env.REDIS_URL, 'process.env.REDIS_URL missing, run `heroku-redis:hobby-dev`');
 
 const jwks = require('./jwks.json');
 
+// require the redis adapter factory/class
+const RedisAdapter = require('./redis_adapter');
+
 const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`, {
+  // configure Provider to use the adapter
+  adapter: RedisAdapter,
   clients: [
     {
       client_id: 'foo',
